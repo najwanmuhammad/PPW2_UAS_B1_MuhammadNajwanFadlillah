351c11254f1ff30fa13c0fb1d55680222c71f74a
najwanmuhammad <158769540+najwanmuhammad@users.noreply.github.com>
2024-12-03 14:10:39 +0700

    bugfix#30

--- resources/views/component/datatable.blade.php
+++ resources/views/component/datatable.blade.php
@@ -1,9 +1,9 @@
-<link rel="stylesheet" href="{{ asset('dataTables.bootstrap5.') }}" />
+<!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/datatables.net-bs5@1.11.5/css/dataTables.bootstrap5.min.css">
 
-
-
-<script src="{{ asset('dataTables.') }}"></script>
-<script src="{{ asset('dataTables.bootstrap5.') }}"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.jsdelivr.net/npm/datatables.net@1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/datatables.net-bs5@1.11.5/js/dataTables.bootstrap5.min.js"></script>
 
 <script>
     $(document).ready(function() {
