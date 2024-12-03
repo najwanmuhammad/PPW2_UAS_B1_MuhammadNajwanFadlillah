8620bef712d78b359461cdc67ea3e0ff3a7fce6c
najwanmuhammad <158769540+najwanmuhammad@users.noreply.github.com>
2024-12-03 13:56:17 +0700

    bugfix#26

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,37 +1,65 @@
-
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
-
         <div class="card">
             <div class="card-header">Login</div>
             <div class="card-body">
-                @if ($message = Session::get('success'))
+                <!-- Flash Messages -->
+                @if (session('success'))
                     <div class="alert alert-success">
-                        {{ $message }}
+                        {{ session('success') }}
                     </div>
                 @endif
-                <form action="{{ route('authenticate') }}" method="post">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <div>{{ $error }}</div>
+                        @endforeach
+                    </div>
+                @endif
+
+                <!-- Login Form -->
+                <form action="{{ route('authenticate') }}" method="POST">
                     @csrf
+                    <!-- Email Field -->
                     <div class="mb-3 row">
                         <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
                         <div class="col-md-6">
-                            <input type="email" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+                            <input
+                                type="email"
+                                class="form-control @error('email') is-invalid @enderror"
+                                id="email"
+                                name="email"
+                                value="{{ old('email') }}"
+                                required
+                                autocomplete="email"
+                                autofocus>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
+
+                    <!-- Password Field -->
                     <div class="mb-3 row">
                         <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
                         <div class="col-md-6">
-                            <input type="password" class="form-control @error('') is-invalid @enderror" name="">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+                            <input
+                                type="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                id="password"
+                                name="password"
+                                required>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
+
+                    <!-- Submit Button -->
                     <div class="mb-3 row">
-                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Login">
+                        <div class="col-md-6 offset-md-4">
+                            <input type="submit" class="btn btn-primary" value="Login">
+                        </div>
                     </div>
                 </form>
             </div>
