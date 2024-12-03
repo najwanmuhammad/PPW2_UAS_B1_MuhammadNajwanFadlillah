4f4b079cb4d0b5d5da5909c9c62919a8a06adf9c
najwanmuhammad <158769540+najwanmuhammad@users.noreply.github.com>
2024-12-03 13:34:28 +0700

    bugfix#13

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -74,7 +74,7 @@ class TransaksiController extends Controller
         return view('transaksi.edit', compact('transaksi'));
     }
 
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
         $request->validate([
             'bayar' => 'required|numeric'
@@ -82,9 +82,10 @@ class TransaksiController extends Controller
 
         $transaksi = Transaksi::findOrFail($id);
         $transaksi->bayar = $request->input('bayar');
-        $transaksi->kembalian =
+        $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga;
+        $transaksi->save();
 
-        return redirect('/transaksi') -> with('pesan', 'Berhasil mengubah data');
+        return redirect('/transaksi')->with('pesan', 'Berhasil mengubah data');
     }
 
     public function destroy()
