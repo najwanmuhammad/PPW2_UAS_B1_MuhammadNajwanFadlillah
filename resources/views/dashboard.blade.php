645828a5ac9ed1c5504756490d9d87e0d5462dda
najwanmuhammad <158769540+najwanmuhammad@users.noreply.github.com>
2024-12-03 13:48:22 +0700

    bugfix#22

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header">SUMMARY</div>
@@ -8,7 +7,11 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Transaksi</h5>
-                            <h4><span class="badge text-bg-secondary">{{ number_format($transaksi_count, 0, '.', '.') }}</span></h4>
+                            <h4>
+                                <span class="badge text-bg-secondary">
+                                    {{ number_format($transaksi_count ?? 0, 0, '.', '.') }}
+                                </span>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -16,7 +19,11 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Item Terjual</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4>
+                                <span class="badge text-bg-secondary">
+                                    {{ number_format($item_terjual ?? 0, 0, '.', '.') }}
+                                </span>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -24,7 +31,11 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Omzet</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4>
+                                <span class="badge text-bg-secondary">
+                                    Rp {{ number_format($omzet ?? 0, 0, ',', '.') }}
+                                </span>
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -36,9 +47,9 @@
         <div class="card-header">DATA</div>
         <div class="card-body">
             <div class="list-group">
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
+                <a href="{{ route('transaksi.create') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
+                <a href="{{ route('transaksi.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
+                <a href="{{ route('transaksidetail.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
             </div>
         </div>
     </div>
